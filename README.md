94d04827a7d9693f28bc0fe6a5c3f4de2b9b8600
mytja <mitja@severkar.eu>
2024-07-05 14:25:41 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# fabric-server-base
-Fabric base with auto-updating mods and auto-updating Fabric/Minecraft versions
+# Fabric Minecraft server base
+> [!WARNING]
+> These are configuration files for hosting my personal servers. Therefore, I will not add any additional mods, unless I deem them important or worth using. Fork the repository and add your own mods to `mods.json` in order to make your own custom template.
+
+> [!CAUTION]
+> By downloading and executing minecraft.jar you automatically agree to Minecraft EULA.
+
+Fabric base with auto-updating mods and auto-updating Fabric/Minecraft versions.
+
+# Why?
+Until now, I had to redownload EVERY. SINGLE. MOD when I wanted to update the version. So I made this.
+
+# Used mods
+- [Anti Xray](https://github.com/DrexHD/AntiXray) by DrexHD
+- [EasyAuth](https://github.com/NikitaCartes/EasyAuth) by NikitaCartes
+- [EasyWhitelist](https://github.com/NikitaCartes/EasyWhitelist) by NikitaCartes
+- [Fabric API](https://github.com/FabricMC/fabric) by modmuss50 and the Fabric team
+- [Fabric Language Kotlin](https://github.com/FabricMC/fabric-language-kotlin) by modmuss50 and the Fabric team
+- [FerriteCore](https://github.com/malte0811/FerriteCore) by malte0811
+- [Universal Graves](https://github.com/Patbox/UniversalGraves) by Patbox
+- [Inv View](https://github.com/PotatoPresident/InvView) by Potatoboy9999/PotatoPresident
+- [Ledger](https://github.com/QuiltServerTools/Ledger) by Potatoboy9999/PotatoPresident, DrexHD, yitzy299 and Quilt Server Tools team
+- [Lithium](https://github.com/caffeinemc/lithium-fabric) by jellysquid3, 2No2Name and CaffeineMC team
+- [LuckPerms](https://github.com/LuckPerms/LuckPerms) by lucko and the LuckPerms team
+- [Memory Leak Fix](https://github.com/fxmorin/memoryLeakFix) by FX/FxMorin
+- [spark](https://github.com/lucko/spark) by lucko
+- [TabTPS](https://github.com/jpenilla/TabTPS) by jpenilla
+
+Have I credited somebody wrong? Perhaps I missed somebody? Please open an issue and I'll fix it promptly.
---
+++ autoupdate.py
@@ -0,0 +1,69 @@
+import requests
+import json
+
+
+def download_file(url, filename):
+    with requests.get(url, stream=True) as req:
+        req.raise_for_status()
+        with open(filename, "wb+") as file:
+            for chunk in req.iter_content(chunk_size=8192):
+                file.write(chunk)
+
+
+# First download the latest Fabric version
+r = requests.get("https://meta.fabricmc.net/v2/versions/")
+res = r.json()
+game_ver = res["game"][0]["version"]
+loader = res["loader"][0]["version"]
+installer = res["installer"][0]["version"]
+
+print(f"Downloading Fabric server for {game_ver}, loader {loader}, installer {installer}")
+
+download_file(f"https://meta.fabricmc.net/v2/versions/loader/{game_ver}/{loader}/{installer}/server/jar", "minecraft.jar")
+
+print("Fabric server download was successful. Located in minecraft.jar.")
+
+downloaded_mods = []
+
+with open("mods.json", "r") as f:
+    mods = json.loads(f.read())
+
+for mod in mods:
+    if mod["id"] in downloaded_mods:
+        print(f"[WARNING] Skipping already downloaded mod {mod['id']}/{mod['filename']}.")
+        continue
+
+    if mod.get("version") is None:
+        mod["version"] = ""
+
+    if mod["source"] == "modrinth":
+        r = requests.get(f'https://api.modrinth.com/v2/project/{mod["id"]}/version?loaders=["fabric"]&game_versions=["{game_ver}"]')
+        if r.status_code == 404:
+            print(f"[ERROR] 404 returned from Modrinth API for mod {mod['id']}/{mod['filename']}.")
+            continue
+
+        res = r.json()
+
+        if len(res) == 0:
+            print(f"[ERROR] Mod ({mod['id']}/{mod['filename']}) doesn't exist for Fabric {game_ver}! Skipping.")
+            continue
+
+        if mod["version"] == res[0]['version_number']:
+            print(f"[INFO] Mod ({mod['id']}/{mod['filename']}) is already updated to the latest version ({mod['version']}). Skipping update!")
+            continue
+
+        print(f"Downloading mod {res[0]['name']} for {res[0]['game_versions']}")
+
+        download_file(res[0]["files"][0]["url"], f'mods/{mod["filename"]}')
+        downloaded_mods.append(mod['id'])
+        mod["version"] = res[0]['version_number']
+
+        print(f"Download for {res[0]['name']} executed successfully!")
+    else:
+        print(f"[ERROR] Mod source {mod['source']} for mod {mod['id']}/{mod['filename']} not recognized!")
+
+print("Updating mods.json!")
+with open("mods.json", "w+") as f:
+    f.write(json.dumps(mods, indent=4))
+
+print("Done!")
